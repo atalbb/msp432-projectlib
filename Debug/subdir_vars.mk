7231cb2f94509aaa8106a0412080015ea9cf15f3
Atal <atal.bajracharya@gmailcom>
2017-06-18 15:18:46 -0400

    1st commit

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,71 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../adc.c \
+../clk.c \
+../debug.c \
+../main.c \
+../printfOverride.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c \
+../systick.c 
+
+C_DEPS += \
+./adc.d \
+./clk.d \
+./debug.d \
+./main.d \
+./printfOverride.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d \
+./systick.d 
+
+OBJS += \
+./adc.obj \
+./clk.obj \
+./debug.obj \
+./main.obj \
+./printfOverride.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj \
+./systick.obj 
+
+OBJS__QUOTED += \
+"adc.obj" \
+"clk.obj" \
+"debug.obj" \
+"main.obj" \
+"printfOverride.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" \
+"systick.obj" 
+
+C_DEPS__QUOTED += \
+"adc.d" \
+"clk.d" \
+"debug.d" \
+"main.d" \
+"printfOverride.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" \
+"systick.d" 
+
+C_SRCS__QUOTED += \
+"../adc.c" \
+"../clk.c" \
+"../debug.c" \
+"../main.c" \
+"../printfOverride.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" \
+"../systick.c" 
+
+
